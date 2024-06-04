f8f79ca293585c8c01fcac663eb25f5c98dcc102
Kwonjaeseong <77simon@naver.com>
2024-06-04 09:44:44 +0900

    init

--- basic/js/08.js
+++ basic/js/08.js
@@ -10,7 +10,7 @@ $('.btn2').on('click', function(){
 
 
 $('.btn2').on(
-    {
+    { 
         'mouseover focus': function(){
             $(this).next().text('안녕하세요')
         }, 
@@ -20,4 +20,14 @@ $('.btn2').on(
     }
 )
 
-//마우스, 키보드 탭키로 포커스 이벤트 둘다 가능.
+//마우스, 키보드 탭키로 포커스 이벤트 둘다 가능. 
+
+
+
+
+// 강제이벤트 발생
+$('.btn1').click()  //이벤트를 바로 발생시켜버림.
+$('.btn2').trigger('mouseover') 
+
+// 이벤트를 제거
+$('.btn2').off('mouseleave')
